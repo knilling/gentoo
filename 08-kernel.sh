83a7ef3495e77c3fed9005c0313c2129a2eec917
Chris Crawford <christopher.p.crawford@gmail.com>
2016-11-01 14:00:31 -0400

    Use the livecd kernel

--- 08-kernel.sh
+++ 08-kernel.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-cp $SCRIPTS/scripts/kernel.config /mnt/gentoo/tmp/
-
+# From https://wiki.gentoo.org/wiki/Gentoo_installation_tips_and_tricks
+# Using the Installation CD kernel
 chroot /mnt/gentoo /bin/bash <<'EOF'
-emerge -vq sys-kernel/gentoo-sources
-cd /usr/src/linux
-mv /tmp/kernel.config .config
-echo 'make'
-make -s
-echo 'make modules_install'
-make -s modules_install
-echo 'make install'
-make -s install
+KN=gentoo
+cp /mnt/cdrom/isolinux/${KN} /mnt/cdrom/isolinux/${KN}.igz /mnt/gentoo/boot
+mkdir -p /mnt/gentoo/lib/modules
+cp -Rp /lib/modules/`uname -r` /mnt/gentoo/lib/modules
+printf "modules=\"" >> /etc/conf.d/modules
+cat /proc/modules | cut -d ' ' -f 1 >> /etc/conf.d/modules
+printf "\"\n" >> /etc/conf.d/modules
 EOF
