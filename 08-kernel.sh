d80a3dc347e755df58d1d0bd444447d33a6e60d6
Chris Crawford <christopher.p.crawford@gmail.com>
2016-11-01 14:17:02 -0400

    This does not need to be in the chroot environment

--- 08-kernel.sh
+++ 08-kernel.sh
@@ -2,7 +2,6 @@
 
 # From https://wiki.gentoo.org/wiki/Gentoo_installation_tips_and_tricks
 # Using the Installation CD kernel
-chroot /mnt/gentoo /bin/bash <<'EOF'
 KN=gentoo
 cp /mnt/cdrom/isolinux/${KN} /mnt/cdrom/isolinux/${KN}.igz /mnt/gentoo/boot
 mkdir -p /mnt/gentoo/lib/modules
@@ -10,4 +9,3 @@ cp -Rp /lib/modules/`uname -r` /mnt/gentoo/lib/modules
 printf "modules=\"" >> /etc/conf.d/modules
 cat /proc/modules | cut -d ' ' -f 1 >> /etc/conf.d/modules
 printf "\"\n" >> /etc/conf.d/modules
-EOF
