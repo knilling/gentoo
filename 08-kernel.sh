ea62e738a81720750cae4cf68036f388e4015f1f
Chris Crawford <christopher.p.crawford@gmail.com>
2016-11-01 17:42:19 -0400

    Build a mega kernel

--- 08-kernel.sh
+++ 08-kernel.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# From https://wiki.gentoo.org/wiki/Gentoo_installation_tips_and_tricks
-# Using the Installation CD kernel
-KN=gentoo
-cp /mnt/cdrom/isolinux/${KN} /mnt/cdrom/isolinux/${KN}.igz /mnt/gentoo/boot
-mkdir -p /mnt/gentoo/lib/modules
-cp -Rp /lib/modules/`uname -r` /mnt/gentoo/lib/modules
-printf "modules=\"" >> /etc/conf.d/modules
-cat /proc/modules | cut -d ' ' -f 1 >> /etc/conf.d/modules
-printf "\"\n" >> /etc/conf.d/modules
+chroot /mnt/gentoo /bin/bash <<'EOF'
+emerge -vq sys-kernel/gentoo-sources
+emerge -vq sys-kernel/genkernel
+genkernel all
+EOF
