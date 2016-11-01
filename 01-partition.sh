7422870655cf45202be3bcbb68fc2974b2bc87bd
Chris Crawford <christopher.p.crawford@gmail.com>
2016-11-01 14:43:11 -0400

    FAT32 needs at least a 512MB partition. Flakey OSes need capital letters

--- 01-partition.sh
+++ 01-partition.sh
@@ -2,7 +2,7 @@
 
 sgdisk \
   -n 1:0:+128M -t 1:8300 -c 1:"linux-boot" \
-  -n 2:0:+32M  -t 2:ef00 -c 2:"bios-boot"  \
+  -n 2:0:+512M -t 2:ef00 -c 2:"bios-boot"  \
   -n 3:0:+1G   -t 3:8200 -c 3:"swap"       \
   -n 4:0:0     -t 4:8300 -c 4:"linux-root" \
   -p /dev/sda
@@ -10,7 +10,7 @@ sgdisk \
 sync
 
 mkfs.ext2 /dev/sda1
-mkfs.vfat -F 32 -n efi-boot /dev/sda2
+mkfs.vfat -F 32 -n EFI-BOOT /dev/sda2
 mkfs.ext4 /dev/sda4
 
 mkswap /dev/sda3 && swapon /dev/sda3
