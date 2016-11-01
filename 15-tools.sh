19401fb910c54c48d5d78224755ee07209e37248
Chris Crawford <christopher.p.crawford@gmail.com>
2016-11-01 18:29:05 -0400

    Create 15-tools.sh

---
+++ 15-tools.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+hroot /mnt/gentoo /bin/bash <<'EOF'
+emerge -vq app-admin/sysklogd
+rc-update add sysklogd default
+emerge -vq sys-process/cronie
+rc-update add cronie default
+emerge -vq sys-apps/mlocate
+rc-update add sshd default
+emerge -vq net-misc/dhcpcd
+EOF
