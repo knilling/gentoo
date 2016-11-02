6f52ef93652e0fce3e4969aecd62f0ad2d01d228
Chris Crawford <christopher.p.crawford@gmail.com>
2016-11-02 12:37:14 -0400

    Add chris to the sudoers list

--- 12-users.sh
+++ 12-users.sh
@@ -5,4 +5,5 @@ USE="-sendmail" emerge -vq app-admin/sudo
 echo root:toor | chpasswd
 useradd -m -G users,wheel -s /bin/bash chris
 echo chris:changeme | chpasswd
+echo 'chris ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/chris
 EOF
