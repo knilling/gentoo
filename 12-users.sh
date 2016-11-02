ad36538155d6f1a1f507b3b5d5ba068e42918adb
Chris Crawford <christopher.p.crawford@gmail.com>
2016-11-02 12:36:07 -0400

    Add a chris user

--- 12-users.sh
+++ 12-users.sh
@@ -3,4 +3,6 @@
 chroot /mnt/gentoo /bin/bash <<'EOF'
 USE="-sendmail" emerge -vq app-admin/sudo
 echo root:toor | chpasswd
+useradd -m -G users,wheel -s /bin/bash chris
+echo chris:changeme | chpasswd
 EOF
