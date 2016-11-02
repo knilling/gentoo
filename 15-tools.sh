0e366cca081270e73585d436d10d24868a88935c
Chris Crawford <christopher.p.crawford@gmail.com>
2016-11-02 13:29:22 -0400

    Add vim and virtualization

--- 15-tools.sh
+++ 15-tools.sh
@@ -8,4 +8,23 @@ rc-update add cronie default
 emerge -vq sys-apps/mlocate
 rc-update add sshd default
 emerge -vq net-misc/dhcpcd
+USE="minimal" emerge vim
+cat > /etc/portage/package.keywords/vagrant <<'DATA'
+=dev-ruby/nokogiri-1.6.8.1 ~amd64
+=dev-ruby/hashicorp-checkpoint-0.1.4-r1 ~amd64
+=dev-ruby/rest-client-1.8.0-r3 ~amd64
+=dev-ruby/bundler-1.12.6 ~amd64
+=dev-ruby/pkg-config-1.1.7 ~amd64
+=dev-ruby/i18n-0.7.0-r2 ~amd64
+=dev-ruby/net-ssh-3.2.0 ~amd64
+=dev-ruby/rb-inotify-0.9.7-r1 ~amd64
+=dev-ruby/ruby_dep-1.5.0 ~amd64
+=app-emulation/vagrant-1.8.6 ~amd64
+=dev-ruby/listen-3.1.5 ~amd64
+DATA
+USE="headless" emerge vagrant
+mkdir -p /etc/portage/package.license
+echo "=app-emulation/virtualbox-extpack-oracle-4.3.38.106717 PUEL" > /etc/portage/package.license/virtualbox-extpack-oracle
+USE="headless" emerge virtualbox-extpack-oracle
+USE="xen virtfs ssh" emerge qemu
 EOF
