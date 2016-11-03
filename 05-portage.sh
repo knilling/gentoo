bcb827968721661b37395e9b1679f510257d2910
Chris Crawford <christopher.p.crawford@gmail.com>
2016-11-03 17:04:16 -0400

    media-libs/libvpx seems to want this flag...

--- 05-portage.sh
+++ 05-portage.sh
@@ -16,7 +16,7 @@ CHOST="x86_64-pc-linux-gnu"
 # These are the USE and USE_EXPAND flags that were used for
 # buidling in addition to what is provided by the profile.
 USE="bindist"
-CPU_FLAGS_X86="aes mcx16 msahf popcnt sse sse2 sse3 sse4.1 sse4.2 ssse3"
+CPU_FLAGS_X86="aes mcx16 msahf popcnt sse sse2 sse3 sse4.1 sse4.2 ssse3 mmx"
 PORTDIR="/usr/portage"
 DISTDIR="${PORTDIR}/distfiles"
 PKGDIR="${PORTDIR}/packages"
