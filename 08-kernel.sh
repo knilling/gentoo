e7bc0bf7e5368ce73afb871e7777e5b89f430b77
Chris Crawford <christopher.p.crawford@gmail.com>
2016-11-08 18:25:03 -0500

    Quick and dirty kernel install

--- 08-kernel.sh
+++ 08-kernel.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# https://wiki.gentoo.org/wiki/Gentoo_installation_tips_and_tricks#Using_the_Installation_CD_kernel
+cp -v /mnt/cdrom/isolinux/gentoo /mnt/gentoo/boot/kernel-gentoo
+cp -v /mnt/cdrom/isolinux/gentoo.igz /mnt/gentoo/boot/initrd-gentoo
+mkdir -p /mnt/gentoo/lib/modules
+cp -Rp /lib/modules/`uname -r` /mnt/gentoo/lib/modules
+
 chroot /mnt/gentoo /bin/bash <<'EOF'
 emerge -vq sys-kernel/gentoo-sources
-emerge -vq sys-kernel/genkernel
-genkernel all
+printf "modules=\"" >> /etc/conf.d/modules
+cat /proc/modules | cut -d ' ' -f 1 >> /etc/conf.d/modules
+printf "\"\n" >> /etc/conf.d/modules
 EOF
