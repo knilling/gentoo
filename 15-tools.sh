7b8b88e41a96ded60957e89f9ae43ccaec23da1e
Chris Crawford <christopher.p.crawford@gmail.com>
2016-11-08 20:11:33 -0500

    Go faster

--- 15-tools.sh
+++ 15-tools.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 chroot /mnt/gentoo /bin/bash <<'EOF'
-emerge -vq app-admin/sysklogd
+emerge -vq -j15 --load-average 8 app-admin/sysklogd
 rc-update add sysklogd default
-emerge -vq sys-process/cronie
+emerge -vq -j15 --load-average 8 sys-process/cronie
 rc-update add cronie default
-emerge -vq sys-apps/mlocate
+emerge -vq -j15 --load-average 8 sys-apps/mlocate
 rc-update add sshd default
-emerge -vq net-misc/dhcpcd
+emerge -vq -j15 --load-average 8 net-misc/dhcpcd
 mkdir -p /etc/portage/package.use/app-editors/
 echo ">=app-editors/vim-7.4.769 minimal" > /etc/portage/package.use/app-editors/vim
-emerge -vq app-editors/vim
-emerge -vq dev-vcs/git
-emerge -vq sys-process/lsof
-emerge -vq sys-process/htop
+emerge -vq -j15 --load-average 8 app-editors/vim
+emerge -vq -j15 --load-average 8 dev-vcs/git
+emerge -vq -j15 --load-average 8 sys-process/lsof
+emerge -vq -j15 --load-average 8 sys-process/htop
 mkdir -p /etc/portage/package.use/app-emulation/
 echo ">=app-emulation/virtualbox-5.1.8 vnc" > /etc/portage/package.use/app-emulation/virtualbox
 cat >> /etc/portage/package.accept_keywords << 'DATA'
@@ -23,7 +23,7 @@ cat >> /etc/portage/package.accept_keywords << 'DATA'
 >=dev-libs/libpcre-8.38-r1 pcre16
 >=app-emulation/virtualbox-5.1.8 ~amd64
 DATA
-emerge -vq app-emulation/virtualbox
+emerge -vq -j15 --load-average 8 app-emulation/virtualbox
 gpasswd -a chris vboxusers
 mkdir -p /etc/portage/package.license/app-emulation/
 echo ">=app-emulation/virtualbox-extpack-oracle-4.3.38.106717 PUEL" > /etc/portage/package.license/app-emulation/virtualbox-extpack-oracle
@@ -42,8 +42,8 @@ cat > /etc/portage/package.keywords/app-emulation/vagrant <<'DATA'
 =app-emulation/vagrant-1.8.6 ~amd64
 =dev-ruby/listen-3.1.5 ~amd64
 DATA
-emerge -vq app-emulation/vagrant
+emerge -vq -j15 --load-average 8 app-emulation/vagrant
 mkdir -p /etc/portage/package.use/app-emulation/
 echo ">=app-emulation/qemu-2.7.0 xen virtfs ssh" > /etc/portage/package.use/app-emulation/qemu
-QEMU_SOFTMMU_TARGETS="x86_64 arm i386" emerge -vq app-emulation/qemu
+QEMU_SOFTMMU_TARGETS="x86_64 arm i386" emerge -vq -j15 --load-average 8 app-emulation/qemu
 EOF
