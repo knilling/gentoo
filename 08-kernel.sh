650b93bc1815030fa94cd647c7e4c167e73edb9f
Chris Crawford <christopher.p.crawford@gmail.com>
2016-11-09 13:12:35 -0500

    Do it the proper way

--- 08-kernel.sh
+++ 08-kernel.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-# https://wiki.gentoo.org/wiki/Gentoo_installation_tips_and_tricks#Using_the_Installation_CD_kernel
-cp -v /mnt/cdrom/isolinux/gentoo /mnt/gentoo/boot/kernel-gentoo
-cp -v /mnt/cdrom/isolinux/gentoo.igz /mnt/gentoo/boot/initrd-gentoo
-zcat /proc/config.gz > /mnt/gentoo/boot/config-gentoo
-mkdir -p /mnt/gentoo/lib/modules
-cp -Rp /lib/modules/`uname -r` /mnt/gentoo/lib/modules
+chroot /mnt/gentoo /bin/bash <<'EOF'
+emerge -qv -j15 --load-average 8 sys-kernel/gentoo-sources
+EOF
+
+zcat /proc/config.gz > /mnt/gentoo/usr/src/linux/.config
 
 chroot /mnt/gentoo /bin/bash <<'EOF'
-emerge -vq sys-kernel/gentoo-sources
-cp -v /boot/config-gentoo /usr/src/linux/.config
 pushd /usr/src/linux
-make oldconfig
-make modules_prepare
+make -j8
+make modules_install
+cp -v arch/x86_64/boot/bzImage /boot/kernel-4.4.26-gentoo
+cp -v .config /boot/config-4.4.26-gentoo
+cp -v System.map /boot/System.map-4.4.26-gentoo
+ln -sf /boot/System.map-4.4.26-gentoo /boot/System.map
 popd
+emerge -qv -j15 --load-average 8 sys-kernel/genkernel
+genkernel --install initramfs
 printf "modules=\"" >> /etc/conf.d/modules
 cat /proc/modules | cut -d ' ' -f 1 >> /etc/conf.d/modules
 printf "\"\n" >> /etc/conf.d/modules
