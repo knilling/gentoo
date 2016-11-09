97a1b9c8570ec34e5d55e0760b441fbaf17d0835
Chris Crawford <christopher.p.crawford@gmail.com>
2016-11-08 19:27:59 -0500

    Update 08-kernel.sh

--- 08-kernel.sh
+++ 08-kernel.sh
@@ -3,6 +3,7 @@
 # https://wiki.gentoo.org/wiki/Gentoo_installation_tips_and_tricks#Using_the_Installation_CD_kernel
 cp -v /mnt/cdrom/isolinux/gentoo /mnt/gentoo/boot/kernel-gentoo
 cp -v /mnt/cdrom/isolinux/gentoo.igz /mnt/gentoo/boot/initrd-gentoo
+zcat /proc/config.gz > /mnt/gentoo/boot/config-gentoo
 mkdir -p /mnt/gentoo/lib/modules
 cp -Rp /lib/modules/`uname -r` /mnt/gentoo/lib/modules
 
